29aa0e0b63470f3238d49383f9ca810ec9c2ceaf
Nikita Mashchenko <mashchenko1@icloud.com>
2024-03-24 06:01:38 -0500

    fix: spacing

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -8,7 +8,7 @@ import Footer from '@/app/(home)/components/Footer';
 export default function page() {
   return (
     <div className='min-h-screen bg-[#09090B] overflow-hidden'>
-      <div className='dark:bg-[#09090B] bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative'>
+      <div className='dark:bg-[#09090B] bg-white dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative'>
         <div className='max-w-7xl mx-auto p-5'>
           <Navbar />
           <Hero />
